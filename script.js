44c5ecbffede602f6a5dbd08a643d2df87c643ea
Alexander Herta <alexanderherta2002@proton.me>
2024-08-02 12:37:00 -0400

    Add a function to return rock, paper, or scissors
    This function selects rock, paper, or scissors at
    random (the computer's choice).

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 10);
+
+  if (randomNumber <= 2) {
+    return "Rock";
+  } else if (randomNumber <= 5 && randomNumber > 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
